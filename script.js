3d714baee24ce2b4dcf60a29a3a456bb583879b6
MagsTek <mags.1317@gmail.com>
2022-11-04 23:55:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/*(function() {
+  const toggleHamburgers = document.getElementsByClassName("toggle-hamburger");
+  console.log(toggleHamburgers);
+})();*/
+
+function toggleHamburger() {
+  const hamburgerMenuContainer = document.getElementById("hamburger-menu-container");
+  console.log(hamburgerMenuContainer);
+  // Agregar o quitar clase de hidden a hamburgerMenuContainer dependiendo el estado actual
+}
